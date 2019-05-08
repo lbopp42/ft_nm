e9bddfe8a3a50843e8748076968232df68072ab7
Lucas BOPP <lbopp@student.42.fr>
2019-05-08 13:17:46 +0200

    Some norme done

--- srcs/nm/ft_nm.c
+++ srcs/nm/ft_nm.c
@@ -6,7 +6,7 @@
 /*   By: lbopp <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/12 10:03:10 by lbopp             #+#    #+#             */
-/*   Updated: 2019/05/03 14:59:24 by lbopp            ###   ########.fr       */
+/*   Updated: 2019/05/08 13:17:25 by lbopp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,32 +22,19 @@ uint32_t	swap_little_endian(uint32_t nb)
 
 int            get_ncmds(void *ptr, t_info info)
 {
-	void                    *header;
 	unsigned int    magic_number;
 	
 	if (info.begin_ptr > ptr || info.begin_ptr + info.size_file < ptr)
 	 	return (0);
 	magic_number = *(int*)ptr;
 	if (magic_number == MH_MAGIC_64)
-	{
-	        header = (struct mach_header_64*)ptr;
-	        return (((struct mach_header_64*)header)->ncmds);
-	}
+	        return (((struct mach_header_64*)ptr)->ncmds);
 	else if (magic_number == MH_MAGIC)
-	{
-	        header = (struct mach_header*)ptr;
-	        return (((struct mach_header*)header)->ncmds);
-	}
+	        return (((struct mach_header*)ptr)->ncmds);
 	else if (magic_number == MH_CIGAM_64)
-	{
-	        header = (struct mach_header_64*)ptr;
-	        return (swap_little_endian(((struct mach_header_64*)header)->ncmds));
-	}
+	        return (swap_little_endian(((struct mach_header_64*)ptr)->ncmds));
 	else if (magic_number == MH_CIGAM)
-	{
-	        header = (struct mach_header*)ptr;
-	        return (swap_little_endian(((struct mach_header*)header)->ncmds));
-	}
+	        return (swap_little_endian(((struct mach_header*)ptr)->ncmds));
 	return (0);
 }
 
@@ -413,19 +400,31 @@ void	browse_load_commands(void *ptr, struct load_command *lc, t_info info)
 	}
 }
 
-void	handle(void *ptr, t_info info)
+void	handle(void *ptr, t_info info, char *filename, int nb_file)
 {
 	struct load_command		*lc;
 
+	if (nb_file > 1)
+	{	
+		ft_putchar('\n');
+		ft_putstr(filename);
+		ft_putendl(":");
+	}
 	lc = (void*)ptr + sizeof(struct mach_header);
 	if (info.begin_ptr <= (void*)lc && info.begin_ptr + info.size_file >= (void*)lc)
 		browse_load_commands(ptr, lc, info);
 }
 
-void	handle_64(void *ptr, t_info info)
+void	handle_64(void *ptr, t_info info, char *filename, int nb_file)
 {
 	struct load_command		*lc;
 
+	if (nb_file > 1)
+	{	
+		ft_putchar('\n');
+		ft_putstr(filename);
+		ft_putendl(":");
+	}
 	lc = (void*)ptr + sizeof(struct mach_header_64);
 	if (info.begin_ptr <= (void*)lc && info.begin_ptr + info.size_file >= (void*)lc)
 		browse_load_commands(ptr, lc, info);
@@ -531,6 +530,47 @@ char	*get_cputype(void *ptr, int i, t_info info)
 	}
 }
 
+void	print_arch_name(struct fat_header *fat_header, char *filename, char *cpu_name)
+{
+	if (swap_little_endian(fat_header->nfat_arch) > 1)
+	{
+		ft_putchar('\n');
+		ft_putstr(filename);
+		ft_putstr(" (for architecture ");
+		ft_putstr(cpu_name);
+		ft_putendl("):");
+	}
+	else
+	{
+		ft_putstr(filename);
+		ft_putendl(":");
+	}
+}
+
+//int		handle_architecture(
+//{
+//	if (fat_header->magic == FAT_CIGAM_64)
+//	{
+//		new_ptr = (void*)ptr + swap_little_endian(fat_arch.is_64->offset);
+//		if (info.begin_ptr > new_ptr || info.begin_ptr + info.size_file < new_ptr)
+//			break ;
+//		ft_nm(new_ptr, swap_little_endian(fat_arch.is_64->size), filename, nb_file, info);
+//		fat_arch.is_64 = (void*)fat_arch.is_64 + sizeof(struct fat_arch_64);
+//		if (info.begin_ptr > (void*)fat_arch.is_64 || info.begin_ptr + info.size_file < (void*)fat_arch.is_64)
+//			break ;
+//	}
+//	else
+//	{
+//		new_ptr = (void*)ptr + swap_little_endian(fat_arch.is_32->offset);
+//		if (info.begin_ptr > new_ptr || info.begin_ptr + info.size_file < new_ptr)
+//			break ;
+//		ft_nm(new_ptr, swap_little_endian(fat_arch.is_32->size), filename, nb_file, info);
+//		fat_arch.is_32 = (void*)fat_arch.is_32 + sizeof(struct fat_arch);
+//		if (info.begin_ptr > (void*)fat_arch.is_32 || info.begin_ptr + info.size_file < (void*)fat_arch.is_32)
+//			break ;
+//	}
+//}
+
 void	process_fat_file(void *ptr, char *filename, int nb_file, t_info info)
 {
 	struct fat_header		*fat_header;
@@ -540,35 +580,14 @@ void	process_fat_file(void *ptr, char *filename, int nb_file, t_info info)
 
 	i = -1;
 	fat_header = ptr;
-	if (fat_header->magic == FAT_CIGAM_64)
-	{
-		fat_arch.is_64 = (struct fat_arch_64*)((void*)ptr + sizeof(struct fat_header));
-		if (info.begin_ptr > (void*)fat_arch.is_64 || info.begin_ptr + info.size_file < (void*)fat_arch.is_64)
-			return ;
-	}
-	else
-	{
-		fat_arch.is_32 = (struct fat_arch*)((void*)ptr + sizeof(struct fat_header));
-		if (info.begin_ptr > (void*)fat_arch.is_32 || info.begin_ptr + info.size_file < (void*)fat_arch.is_32)
-			return ;
-	}
 	while (++i < swap_little_endian(fat_header->nfat_arch))
 	{
-		if (swap_little_endian(fat_header->nfat_arch) > 1)
-		{
-			ft_putchar('\n');
-			ft_putstr(filename);
-			ft_putstr(" (for architecture ");
-			ft_putstr(get_cputype(ptr, i, info));
-			ft_putendl("):");
-		}
-		else
-		{
-			ft_putstr(filename);
-			ft_putendl(":");
-		}
+		print_arch_name(fat_header, filename, get_cputype(ptr, i, info));
 		if (fat_header->magic == FAT_CIGAM_64)
 		{
+			fat_arch.is_64 = (struct fat_arch_64*)((void*)ptr + sizeof(struct fat_header));
+			if (info.begin_ptr > (void*)fat_arch.is_64 || info.begin_ptr + info.size_file < (void*)fat_arch.is_64)
+				return ;
 			new_ptr = (void*)ptr + swap_little_endian(fat_arch.is_64->offset);
 			if (info.begin_ptr > new_ptr || info.begin_ptr + info.size_file < new_ptr)
 				break ;
@@ -579,6 +598,9 @@ void	process_fat_file(void *ptr, char *filename, int nb_file, t_info info)
 		}
 		else
 		{
+			fat_arch.is_32 = (struct fat_arch*)((void*)ptr + sizeof(struct fat_header));
+			if (info.begin_ptr > (void*)fat_arch.is_32 || info.begin_ptr + info.size_file < (void*)fat_arch.is_32)
+				return ;
 			new_ptr = (void*)ptr + swap_little_endian(fat_arch.is_32->offset);
 			if (info.begin_ptr > new_ptr || info.begin_ptr + info.size_file < new_ptr)
 				break ;
@@ -603,6 +625,35 @@ void	handle_fat_file(const void *ptr, char *filename, int nb_file, t_info info)
 		process_fat_file((void*)ptr, filename, nb_file, info);
 }
 
+void	print_ar_name(int size_name, struct ar_hdr *ar, t_info info)
+{
+	if (info.begin_ptr > (void*)ar + sizeof(*ar)
+		|| info.begin_ptr + info.size_file < (void*)ar + sizeof(*ar))
+		return ;
+	if (size_name != 0)
+	{
+		ft_putstr("(");
+		if (info.begin_ptr > (void*)ar || info.begin_ptr + info.size_file < (void*)ar)
+			return ;
+		ft_putstr(ar->ar_name + sizeof(struct ar_hdr));
+		ft_putendl("):");
+	}
+	else
+	{
+		ft_putstr("(");
+		ft_putstr(ar->ar_name);
+		ft_putendl("):");
+	}
+}
+
+void	launch_nm(struct ar_hdr *ar, unsigned int size_name, char *filename, t_info info)
+{
+	if (info.begin_ptr > (void*)ar + sizeof(struct ar_hdr) + size_name
+			|| info.begin_ptr + info.size_file < (void*)ar + sizeof(struct ar_hdr) + size_name)
+		return ;
+	ft_nm((void*)ar + sizeof(struct ar_hdr) + size_name, ft_atoi(ar->ar_size), filename, -1, info);
+}
+
 void	handle_arch(void *ptr, int size, char *filename, t_info info)
 {
 	struct ar_hdr	*ar;
@@ -623,25 +674,9 @@ void	handle_arch(void *ptr, int size, char *filename, t_info info)
 		size -= sizeof(struct ar_hdr) + ft_atoi(ar->ar_size);
 		if (!ft_strncmp(ar->ar_name, "#1/", 3))
 			size_name = ft_atoi(ar->ar_name + 3);
-		if (size_name != 0)
-		{
-			ft_putstr("(");
-			ft_putstr(ar->ar_name + sizeof(struct ar_hdr));
-			ft_putendl("):");
-		}
-		else
-		{
-			ft_putstr("(");
-			ft_putstr(ar->ar_name);
-			ft_putendl("):");
-		}
+		print_ar_name(size_name, ar, info);
 		if (ft_strcmp(ar->ar_name, ""))
-		{
-			if (info.begin_ptr > (void*)ar + sizeof(struct ar_hdr) + size_name
-					|| info.begin_ptr + info.size_file < (void*)ar + sizeof(struct ar_hdr) + size_name)
-				return ;
-			ft_nm((void*)ar + sizeof(struct ar_hdr) + size_name, ft_atoi(ar->ar_size), filename, -1, info);
-		}
+			launch_nm(ar, size_name, filename, info);
 		ar = (void*)ar + sizeof(struct ar_hdr) + ft_atoi(ar->ar_size);
 		if (info.begin_ptr > (void*)ar || info.begin_ptr + info.size_file < (void*)ar)
 			return ;
@@ -657,25 +692,9 @@ void	ft_nm(char *ptr, int size, char *filename, int nb_file, t_info info)
 		return ;
 	magic_number = *(int *)ptr;
 	if (magic_number == MH_MAGIC_64 || magic_number == MH_CIGAM_64)
-	{
-		if (nb_file > 1)
-		{	
-			ft_putchar('\n');
-			ft_putstr(filename);
-			ft_putendl(":");
-		}
-		handle_64(ptr, info);
-	}
+		handle_64(ptr, info, filename, nb_file);
 	else if (magic_number == MH_MAGIC || magic_number == MH_CIGAM)
-	{
-		if (nb_file > 1)
-		{
-			ft_putchar('\n');
-			ft_putstr(filename);
-			ft_putendl(":");
-		}
-		handle(ptr, info);
-	}
+		handle(ptr, info, filename, nb_file);
 	else if (is_fat_file(ptr, info))
 		handle_fat_file(ptr, filename, nb_file, info);
 	else if (!ft_memcmp(ptr, ARMAG, SARMAG))
@@ -683,32 +702,17 @@ void	ft_nm(char *ptr, int size, char *filename, int nb_file, t_info info)
 	i++;
 }
 
-int	*try_open(char *filename, int nb_file)
+int	try_open(char *filename)
 {
-	int *fd;
-	int	i;
+	int fd;
 
-	if (!(fd = malloc(sizeof(int) * nb_file)))
-		return (NULL);
-	i = 0;
-	while (i < nb_file)
+	if ((fd = open(filename, O_RDONLY)) < 0)
 	{
-		if ((fd[i] = open(filename, O_RDONLY)) < 0)
-		{
-			if (errno == 13)
-				ft_putendl_fd("ft_nm: Permission Denied.", 2);
-			else
-				ft_putendl_fd("ft_nm: No such file or directory.", 2);
-			while (i >= 0)
-			{
-				close(fd[i]);
-				i--;
-			}
-			free(fd);
-			return (NULL);
-		}
-		filename += ft_strlen(filename) + 1;
-		i++;
+		if (errno == 13)
+			ft_putendl_fd("ft_nm: Permission Denied.", 2);
+		else
+			ft_putendl_fd("ft_nm: No such file or directory.", 2);
+		return (0);
 	}
 	return (fd);
 }
@@ -730,53 +734,57 @@ int	get_stat_file(char *filename, int fd, struct stat *buf)
 	return (0);
 }
 
-int	close_fd_error(int **fd, int nb_file, int return_nb)
+int	close_fd_error(int fd, int return_nb)
 {
-	while (nb_file - 1 >= 0)
-	{
-		close((*fd)[nb_file - 1]);
-		nb_file--;
-	}
-	free(*fd);
+	close(fd);
 	return (return_nb);
+}
+
+int	nm_no_arg(void)
+{
+	int			fd;
+	struct stat	buf;
+	t_info		info;
+	char		*ptr;
+
+	if (!(fd = try_open("a.out")))
+		return(1);
+	if (get_stat_file("a.out", fd, &buf) == 1)
+		return (close_fd_error(fd, 1));
+	if ((ptr = mmap(0, buf.st_size, PROT_READ, MAP_PRIVATE, fd, 0)) == MAP_FAILED)
+		return (1);
+	info.begin_ptr = ptr;
+	info.size_file = buf.st_size;
+	ft_nm(ptr, buf.st_size, "a.out", 1, info);
+	if ((munmap(ptr, buf.st_size)) == -1)
+		return (close_fd_error(fd, 1));
+	return (0);
 }
 
 int	main(int ac, char **av)
 {
-	int			*fd;
+	int			fd;
 	struct stat	buf;
 	char		*ptr;
 	int			i;
 	t_info		info;
 
-	if (!av[1] && !(fd = try_open("a.out", 1)))
-		return(1);
-	if (av[1] && !(fd = try_open(av[1], ac - 1)))
-		return (1);
+	if (!av[1])
+		return (nm_no_arg());
 	i = 0;
-	while (i < ac - 1 || (i == 0 && !av[1]))
+	while (++i < ac)
 	{
-		if (av[1] && get_stat_file(av[i + 1], fd[i], &buf) == 1)
-			return (close_fd_error(&fd, ac - 1, 1));
-		else if (!av[1] && get_stat_file("a.out", fd[i], &buf) == 1)
-			return (close_fd_error(&fd, 1, 1));
-		if ((ptr = mmap(0, buf.st_size, PROT_READ, MAP_PRIVATE, fd[i], 0)) == MAP_FAILED)
-		{
-			//printf("MAP FAILED"); //TODO ft_nm <(cat /bin/ls) FAIL !
-			return (close_fd_error(&fd, ac - 1, 1));
-		}
+		if (!(fd = try_open(av[i])))
+			return (1);
+		if (get_stat_file(av[i], fd, &buf) == 1)
+			return (close_fd_error(fd, 1));
+		if ((ptr = mmap(0, buf.st_size, PROT_READ, MAP_PRIVATE, fd, 0)) == MAP_FAILED)
+			return (close_fd_error(fd, 1));
 		info.begin_ptr = ptr;
 		info.size_file = buf.st_size;
-		if (!av[1])
-			ft_nm(ptr, buf.st_size, "a.out", 1, info);
-		else
-			ft_nm(ptr, buf.st_size, av[i + 1], ac - 1, info);
+		ft_nm(ptr, buf.st_size, av[i], ac - 1, info);
 		if ((munmap(ptr, buf.st_size)) == -1)
-		{
-			//printf("ERREUR MUNMAP"); //TODO
-			return (close_fd_error(&fd, ac - 1, 1));
-		}
-		i++;
+			return (close_fd_error(fd, 1));
 	}
-	return (close_fd_error(&fd, ac - 1, 0));
+	return (close_fd_error(fd, 0));
 }
